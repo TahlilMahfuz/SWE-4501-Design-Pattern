633d3085147e920da0ea52c294e9966f1c4afb4a
Tahlil_Mahfuz <tahlilkfaiyaz@gmail.com>
2023-11-09 15:42:12 +0600

    Uploaded progress Lab_final_Racing_At_IUT

---
+++ Lab_Final_Racing_In_IUT/src/Car.java
@@ -0,0 +1,39 @@
+abstract class Car {
+    private String name;
+    private Engine engine;
+    private TurboCharger turboCharger;
+    public Car(String name,Engine engine,TurboCharger turboCharger){
+        this.name=name;
+        this.engine=engine;
+        this.turboCharger=turboCharger;
+    }
+    public void setEngine(Engine engine){
+        this.engine=engine;
+    }
+    public void setTurboCharger(TurboCharger turboCharger){
+        this.turboCharger=turboCharger;
+    }
+    //template Pattern
+    public void startRace(){
+        start();
+        accelerate();
+        shiftgear();
+        stop();
+    }
+    public void showCarInfo() {
+        System.out.println("Car name: " + name);
+        engine.showEngineInfo();
+        turboCharger.showTurboChargerInfo();
+    }
+    //Hook method
+    public void start(){
+        System.out.println("Car is starting...");
+    }
+    public void stop(){
+        System.out.println("Car is stopping...");
+    }
+    public void accelerate(){
+        System.out.println("Car is accelerating...");
+    }
+    abstract void shiftgear();
+}
